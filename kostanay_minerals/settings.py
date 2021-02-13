b8685742ba67bed678d034dc8161afb89e0b4aff
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2021-02-13 12:53:24 +0600

    update

--- kostanay_minerals/settings.py
+++ kostanay_minerals/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%nyh_yf-oc(%*w(@h548d5t*93+!0@gd&^9eqexj6-$!mk3rtv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,4 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = Path(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
+
+
+# STATICFILES_DIRS = [Path(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR, 'static/media')
